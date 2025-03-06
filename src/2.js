fd65c93e0eaa1f394c0e44d9dbead6e4095293f3
dardenls86 <dardenls86@gmx.com>
2025-03-06 17:22:50 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,10 @@
+let x = Math.floor(Math.random() * 10);
+let y = Math.floor(Math.random() * 10);
+
+if (x > y) {
+  console.log("x is greater than y");
+} else if (y > x) {
+  console.log("y is greater than x");
+} else {
+  console.log("x and y are equal");
+}
